3053f52cdc60476e7fb9c4bc2a759074ae15a741
Keiichi-S <keiichidori1222@gmail.com>
2022-06-15 00:14:25 +0900

    can chat cheaply

--- application/app.py
+++ application/app.py
@@ -2,6 +2,7 @@ import os
 from application.db.connect import get_connection
 from flask import Flask, render_template
 from datetime import timedelta
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 
 app = Flask(__name__)
 
@@ -14,6 +15,7 @@ app.config.update(
 app.config['JSON_AS_ASCII'] = False
 app.secret_key = os.environ["SECRET_KEY"]
 app.permanent_session_lifetime = timedelta(hours=12)
+socketio = SocketIO(app, logger=True, engineio_logger=True, cors_allowed_origins='*')
 
 @app.route("/")
 def index():
@@ -28,3 +30,21 @@ def get_users():
     cur.close()
     
     return users
+
+@socketio.on("join")
+def on_join(data):
+    username = data["username"]
+    room = data["room"]
+    join_room(room)
+    send(username + " has entered the room.", to=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
+
+@socketio.on("message")
+def handle_message(room, message):
+    send(message, to=room)
