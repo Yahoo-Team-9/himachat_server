a798cc2e73d972f22c24067b5df87136ef72841e
Keiichi-S <keiichidori1222@gmail.com>
2022-06-17 00:43:38 +0900

    can add additional members

--- application/app.py
+++ application/app.py
@@ -30,8 +30,9 @@ def get_users():
     
     return users
 
-@app.route("/get_group", methods=["GET"])
-def get_group():
+#指定したグループのメンバー取得
+@app.route("/get_group_members", methods=["GET"])
+def get_group_members():
     # group_id = 37
     group_id = request.json["group_id"]
     conn = get_connection()
@@ -43,6 +44,7 @@ def get_group():
 
     return {"members": members}
     
+#グループ作成&指定したメンバーを追加
 @app.route("/create_group", methods=["POST"])
 def create_group():
     # primary_user_id = 1
@@ -54,17 +56,33 @@ def create_group():
     cur.execute(sql)
     conn.commit()
 
+
     cur.execute('select last_insert_id();')
     group_id = cur.fetchone()
+    cur.close()
 
     
     # added_members = [2, 3, 4]
     added_members = ast.literal_eval(request.json["added_members"]) # "[2,3]" -> [2,3]
-    for member in [primary_user_id] + added_members:
-        sql = f'insert into group_users(group_id, primary_user_id) values({group_id[0]}, {member})'
+    add_member(group_id[0], [primary_user_id] + added_members)
+    
+    return jsonify(res="ok")
+
+#指定したグループに指定したメンバーを追加
+@app.route("/update_group", methods=["POST"])
+def update_group():
+    group_id = request.json["group_id"]
+    added_members = ast.literal_eval(request.json["added_members"]) # "[2,3]" -> [2,3]
+    add_member(group_id, added_members)
+    
+    return jsonify(res="ok")
+
+#グループにメンバーを追加
+def add_member(group_id, added_members):
+    conn = get_connection()
+    cur = conn.cursor()
+    for member in added_members:
+        sql = f'insert into group_users(group_id, primary_user_id) values({group_id}, {member})'
         cur.execute(sql)
         conn.commit()
-
-
     cur.close()
-    return jsonify(res="ok")
