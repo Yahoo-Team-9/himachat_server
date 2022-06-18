6d5925097c09fc3c77408ce73caef9a833fd8132
hikaru0112 <h.sato.sys19@morijyobi.ac.jp>
2022-06-18 12:23:27 +0900

    blueprintの登録

--- application/app.py
+++ application/app.py
@@ -1,6 +1,8 @@
 import os
+
+from application.api.follow import follow
 from application.db.connect import get_connection
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -15,8 +17,13 @@ app.config['JSON_AS_ASCII'] = False
 app.secret_key = os.environ["SECRET_KEY"]
 app.permanent_session_lifetime = timedelta(hours=12)
 
+
+app.register_blueprint(follow)
+
 @app.route("/")
 def index():
+    #TODO: 仮セッション
+    session['user'] = '1'
     return render_template('sample.html',users=get_users())
 
 def get_users():
