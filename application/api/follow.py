d832c1b071d66c13eb00ae795911e0cfee16d12d
hikaru0112 <h.sato.sys19@morijyobi.ac.jp>
2022-06-18 12:23:05 +0900

    follow機能の作成

--- application/api/follow.py
+++ application/api/follow.py
@@ -1,2 +1,26 @@
-# 仮ファイル
+from flask import Flask, Blueprint, render_template, request, redirect, jsonify, session
+
+from application.db.follow import set_follow_db
+
+follow = Blueprint('home', __name__, url_prefix='/api/follow')
+
+
+@follow.route('/get_follow_list')
+def get_follow_list():
+    return  'a'
+
+
+@follow.route('/get_follower_list')
+def get_follower_list():
+    return  'a'
+
+@follow.route('/follow', methods=["POST"])
+def set_follow():
+    #if session not in session
+    follow_id = request.form.get('user_id')
+    if set_follow_db(session['user'],follow_id):
+        return jsonify({"status": "OK"}), 200
+    else:
+        return  jsonify({"status","not followed"}),413
+
 
