d68402c2212e13e9377fb822a4cf7ac6f368fd99
hikaru0112 <h.sato.sys19@morijyobi.ac.jp>
2022-06-19 21:14:18 +0900

    ログイン時間更新処理の作成

---
+++ application/db/leisure.py
@@ -0,0 +1,13 @@
+from application.db.connect import get_connection
+
+
+def set_leisure_time(primary_user_id) -> int:
+    #TODO: エラー処理をする
+    conn = get_connection()
+    cur = conn.cursor()
+    sql = 'UPDATE users SET login_at = NOW() WHERE primary_user_id = %s;'
+    cur.execute(sql, (primary_user_id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return 0
