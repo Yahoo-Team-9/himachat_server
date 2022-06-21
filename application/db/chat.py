2e55dbea9f160e26a90276506c75b08b0ec0b2e8
Keiichi-S <keiichidori1222@gmail.com>
2022-06-21 13:28:03 +0900

    modify sql command

--- application/db/chat.py
+++ application/db/chat.py
@@ -1,17 +1,18 @@
+import MySQLdb
 from application.db.connect import get_connection
 
 def insert_message_db(chatRoom, primary_user_id, message):
     conn = get_connection()
     cur = conn.cursor()
-    sql = f'insert into chats(group_id, send_user, message) values({chatRoom}, {primary_user_id}, "{message}")'
-    cur.execute(sql)
+    sql = 'insert into chats(group_id, send_user, message) values(%s, %s, %s)'
+    cur.execute(sql, (chatRoom, primary_user_id, message))
     conn.commit()
 
 def get_group_messages_db(group_id):
     conn = get_connection()
-    cur = conn.cursor()
-    sql = f'select send_user, created_at, message from chats where group_id={group_id}'
-    cur.execute(sql)
+    cur = conn.cursor(MySQLdb.cursors.DictCursor)
+    sql = 'select send_user, created_at, message from chats where group_id = %s'
+    cur.execute(sql, (group_id, ))
     message_history = cur.fetchall()
     cur.close()
     return message_history
--- application/db/group.py
+++ application/db/group.py
@@ -1,11 +1,12 @@
+import MySQLdb
 from application.db.connect import get_connection
 
 
 def create_group_db(primary_user_id):
     conn = get_connection()
     cur = conn.cursor()
-    sql = f'insert into user_groups(group_owner) values({primary_user_id})'
-    cur.execute(sql)
+    sql = 'insert into user_groups(group_owner) values(%s)'
+    cur.execute(sql, (primary_user_id, ))
     conn.commit()
 
     cur.execute('select last_insert_id();')
@@ -19,16 +20,16 @@ def add_member_db(group_id, added_members):
     conn = get_connection()
     cur = conn.cursor()
     for member in added_members:
-        sql = f'insert into group_users(group_id, primary_user_id) values({group_id}, {member})'
-        cur.execute(sql)
+        sql = 'insert into group_users(group_id, primary_user_id) values(%s, %s)'
+        cur.execute(sql, (group_id, member))
         conn.commit()
     cur.close()
 
 def get_group_members_db(group_id):
     conn = get_connection()
-    cur = conn.cursor()
-    sql = f'select primary_user_id from group_users where group_id={group_id}'
-    cur.execute(sql)
+    cur = conn.cursor(MySQLdb.cursors.DictCursor)
+    sql = 'select primary_user_id from group_users where group_id = %s'
+    cur.execute(sql, (group_id,))
     members = cur.fetchall()
     cur.close()
 
