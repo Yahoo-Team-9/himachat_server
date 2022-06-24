e230ec93289fbfa2a32dc119a080cb73510dcd7b
ebisenpai <satsukiabe0328@gmail.com>
2022-06-24 18:50:18 +0900

    画像ファイルアップロード

--- application/api/user.py
+++ application/api/user.py
@@ -1,5 +1,13 @@
-from flask import Flask, Blueprint, render_template, request, redirect, jsonify, session
+import os
+from flask import Flask, Blueprint, render_template, request, redirect, jsonify, session, flash, current_app, url_for
 from application.db.user import get_profile_db, edit_profile_db
+from werkzeug.utils import secure_filename
+from pathlib import Path
+
+path = Path(__file__).parent
+path /= '../static/img/user_icon'
+UPLOAD_FOLDER = path
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 
 user = Blueprint('user', __name__, url_prefix='/api/user')
 
@@ -32,3 +40,26 @@ def edit_profile():
     edited_profile = request.json["edited_profile"]
     edit_profile_db(primary_user_id, edited_profile)
     return jsonify(res="ok")
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+#画像ファイルのアップロード
+@user.route('/upload_file', methods=['POST'])
+def upload_file():
+    current_app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            return jsonify(res="ok") #redirect(url_for('download_file', name=filename))
+    return jsonify(res="ok")
+
